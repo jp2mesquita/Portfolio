8243b44d617fea1be1d097c376633290320b5eba
João Paulo Mesquita <jp-mesquita@live.com>
2022-09-26 15:32:11 -0300

    Initial commit already with styled-components set

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+
+  h1{
+    color: red;
+  }
+`
